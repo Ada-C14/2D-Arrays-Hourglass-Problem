52b81ab1e86850339d72741ebe2458a270419d25
Pauline Chane <pauline.chane@gmail.com>
2020-12-14 15:46:24 -0800

    added edge case catch

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,7 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  # can we assume valid input?
+  # assuming anything that isn't in a 3by3 is invalid input
+  # let's just return 0 since it isn't an hour glass to begin with
+  return 0 if matrix.length < 3 || matrix[0].length < 3
 end

49efdb60f026ddc1f290ab5e6c3128570ccce642
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-15 12:10:07 -0800

    added method for hourglass and passed the tests

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,23 @@
-
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  start = 0
+  row = 3
+  largest_sum = nil
+  while row < matrix.length + 1
+    col = 3
+    while col < matrix[0].length + 1
+      temp_sum = hourglass_helper(matrix[start...row], col)
+      largest_sum = temp_sum if largest_sum.nil? || largest_sum < temp_sum 
+      col += 1
+    end
+    start += 1
+    row += 1
+  end
+  return largest_sum
+end
+
+def hourglass_helper(row, col)
+  row0 = row[0][col-3...col].sum
+  row1 = row[1][col-3...col][1]
+  row2 = row[2][col-3...col].sum
+  return row0 + row1 + row2
 end

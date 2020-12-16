b2661621cc4bce177963e815e304ef69193d27e2
Lina Do <l.do5147@gmail.com>
2020-12-16 15:22:02 -0800

    solved the hour glass problem

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,23 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+
+  return nil if matrix.length < 3 || matrix[0].length < 3
+
+  sum_array = []
+  rows = matrix.length - 2
+  columns = matrix[0].length - 2
+
+  rows.times do |row|
+    columns.times do |column|
+      sum = 0
+      3.times do |i|
+        sum = sum + matrix[row][column + i] + matrix[row + 2][column + i]
+      end
+
+      sum_array << sum + matrix[row + 1][column + 1]
+    end
+  end
+
+  return sum_array.max
+
 end

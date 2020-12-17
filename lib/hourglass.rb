175a254a3c2b8d992d450f7033e7c85a0397853b
Emily Nagai <emnagai89@gmail.com>
2020-12-16 16:31:25 -0800

    hourglass first draft

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,16 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  sum = 0
+  max_sum = -1000
+
+  4.times do |row|
+    4.times do |col|
+      sum = matrix[row][col] +
+          matrix[row][col + 1] + matrix[row][col + 2] + matrix[row + 1][col + 1]+ matrix[row + 2][col] + matrix[row +2][col + 1] + matrix[row + 2][col + 2]
+
+       max_sum = sum if sum > max_sum
+      end
+    end
+
+  return max_sum
 end

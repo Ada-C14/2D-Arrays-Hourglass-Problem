0cb23f84ddc032c46af2cb1581f378d04dcad2cf
roshni-patel <rpatel01@wesleyan.edu>
2020-12-17 15:00:04 -0600

    First attempt at solving hourglass sum, all tests passing

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,18 @@
-
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
-end
+    row = 0
+    max_sum = -63
+
+  while matrix[row + 2]
+    column = 0
+    while matrix[row][column + 2]
+      current_sum = matrix[row][column] + matrix[row][column + 1] + matrix[row][column + 2] +
+          matrix[row + 1][column + 1] +
+          matrix[row + 2][column] + matrix[row + 2][column + 1] + matrix[row + 2][column + 2]
+
+      max_sum = current_sum if current_sum > max_sum
+      column += 1
+    end
+    row += 1
+  end
+  return max_sum
+ end

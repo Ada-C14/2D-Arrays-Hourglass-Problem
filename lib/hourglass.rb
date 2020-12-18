9f78cdcc80e28d8a18cc5186603e13bcaf511172
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-18 14:41:02 -0800

    define hourglass method

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,21 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  sum = -1.0/0
+  row_start = 0
+  column_start = 0
+
+  while row_start < matrix.length - 2
+    while column_start < matrix[0].length - 2
+      hourglass = matrix[row_start][column_start..column_start + 2].sum
+      hourglass += matrix[row_start + 1][column_start + 1]
+      hourglass += matrix[row_start + 2][column_start..column_start + 2].sum
+
+      sum = hourglass if hourglass > sum
+      column_start += 1
+    end
+    row_start += 1
+    column_start = 0
+  end
+
+  return sum
 end

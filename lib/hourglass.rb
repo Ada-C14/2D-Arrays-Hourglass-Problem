14e62213a16f898d9faa5e75909a04a8df528023
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-18 17:36:07 -0800

    add hourglass solution

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,27 @@
+# Time Complexity - O(1) because matrix is fixed and there's a fixed number of iterations.
+# Space Complexity - O(1)
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  max_sum = -Float::INFINITY
+  row_length = matrix.length
+  col_length = matrix[0].length
+
+  (row_length - 2).times do |row|
+    (col_length - 2).times do |col|
+      current_sum = 0
+
+      (0..2).each do |i|
+        current_sum += matrix[row][col + i] # sum top of hourglass
+        current_sum += matrix[row + 2][col + i] # sum bottom of hourglass
+      end
+
+      current_sum += matrix[row + 1][col + 1] # sum middle of hourglass
+
+      if current_sum > max_sum
+        max_sum = current_sum
+      end
+    end
+  end
+
+  return max_sum
 end

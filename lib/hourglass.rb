7a0fa95a3249c38540480713ec6f9551a6b83971
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-18 19:00:42 -0800

    add complexity comments

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,20 +1,22 @@
 
+# time complexity: O(n*m)
+# space complexity: O(1)
 def hourglass(matrix)
   sum = -1.0/0
-  row_start = 0
-  column_start = 0
+  top = 0
+  left = 0
 
-  while row_start < matrix.length - 2
-    while column_start < matrix[0].length - 2
-      hourglass = matrix[row_start][column_start..column_start + 2].sum
-      hourglass += matrix[row_start + 1][column_start + 1]
-      hourglass += matrix[row_start + 2][column_start..column_start + 2].sum
+  while top < matrix.length - 2
+    while left < matrix[0].length - 2
+      hourglass = matrix[top][left] + matrix[top][left + 1] + matrix[top][left + 2]
+      hourglass += matrix[top + 1][left + 1]
+      hourglass += matrix[top + 2][left] + matrix[top + 2][left + 1] + matrix[top + 2][left + 2]
 
       sum = hourglass if hourglass > sum
-      column_start += 1
+      left += 1
     end
-    row_start += 1
-    column_start = 0
+    top += 1
+    left = 0
   end
 
   return sum

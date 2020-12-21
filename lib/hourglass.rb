851cf79ac98ada60eb6a410c487a03a85d0a99e9
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-21 11:14:01 -0800

    Initial commit. Just running through class problem to wrap head around new approach

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,32 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  return nil if matrix.empty?
+
+  highest_sum = 0
+  index = 0
+
+  matrix.length.times do |row|
+    matrix[row].length.times do |col|
+      total = 0
+      total += matrix[row][col]
+      if total > highest_sum
+        pp highest_sum
+        highest_sum = total
+      end
+
+    end
+  end
+
+  return highest_sum
 end
+
+matrix = [
+    [1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+]
+
+pp hourglass(matrix)

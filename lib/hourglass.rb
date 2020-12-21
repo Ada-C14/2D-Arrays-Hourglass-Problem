f18abdced3fa4f1eedaadb8feacb6e17c50ed305
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-21 12:28:47 -0800

    all tests passing

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,32 +1,33 @@
 
 def hourglass(matrix)
-  return nil if matrix.empty?
+  return 0 if matrix.empty?
 
-  highest_sum = 0
-  index = 0
+  highest_sum = -63 #highest negative number possible is -9...7 values are added, so -9* 7 is -63. That's the highest neg. value we would come across.
 
-  matrix.length.times do |row|
-    matrix[row].length.times do |col|
+  4.times do |row| # sum arrays will be 4 x 4
+    4.times do |column|
       total = 0
-      total += matrix[row][col]
+      total += (matrix[row][column] + matrix[row][column + 1] + matrix[row][column + 2] + #top row
+       matrix[row + 1][column + 1] + #middle row
+      matrix[row + 2][column] + matrix[row + 2][column + 1] + matrix[row + 2][column + 2]) #bottom row
       if total > highest_sum
-        pp highest_sum
         highest_sum = total
       end
-
     end
   end
 
   return highest_sum
 end
 
-matrix = [
-    [1, 1, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-]
+# matrix = [
+#     [1, 1, 1, 0, 0, 0],
+#     [0, 1, 0, 0, 0, 0],
+#     [1, 1, 1, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0],
+# ]
+#
+# pp hourglass(matrix)
 
-pp hourglass(matrix)
+

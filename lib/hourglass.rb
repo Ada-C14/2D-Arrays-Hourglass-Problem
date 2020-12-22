2bfa2d43933289df49e9ff80ad9ea77b3e98ceb0
Stacy <lundquist.stacy@gmail.com>
2020-12-21 18:05:51 -0800

    solved, all tests passing

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,20 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  sum = -100
+  i = 1
+  (matrix.length - 2).times do
+    j = 1
+    (matrix[0].length - 2).times do
+      temp_sum = matrix[i][j] + matrix[i - 1][j - 1] + matrix[i - 1][j] + matrix[i - 1][j + 1] + matrix[i + 1][j - 1] + matrix[i + 1][j] + matrix[i + 1][j + 1]
+      if sum < temp_sum
+        sum = temp_sum
+      end
+      
+      j += 1
+    end
+
+    i += 1
+  end
+  return sum
+
 end

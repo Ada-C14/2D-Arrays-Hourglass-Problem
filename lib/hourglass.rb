5014c7a3c02626c2e667965054b3ae3330c78d5a
Tram Bui <pbui17@cmc.edu>
2020-12-22 09:05:02 -0800

    finished the hourglass problem

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,17 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  # raise NotImplementedError, "The matrix method has not been implemented yet!"
+  sum = []
+  iterations = matrix.size - 2
+  iterations.times do |i|
+    iterations.times do |j|
+      sum << (
+        matrix[i][j] + matrix[i][j+1] + matrix[i][j+2] +
+        matrix[i+1][j+1] +
+        matrix[i+2][j] + matrix[i+2][j+1] + matrix[i+2][j+2]
+      )
+    end
+  end
+
+  return sum.max
 end

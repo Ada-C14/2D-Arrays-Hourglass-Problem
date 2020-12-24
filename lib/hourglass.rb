a3f326794dc73171ca02998ab2102b9782f9f029
Marj E <marj@marjorieetienne.com>
2020-12-23 23:16:59 -0800

    added first solution to hourglass

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,27 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  max_hourglass_sum = - 1.0/0
+  max_iteration_count = matrix.length - 2 # assuming this only works because matrix is even
+
+  row_base = 0
+  col_base = 0
+
+  while row_base < max_iteration_count && col_base < max_iteration_count
+    4.times do |i|
+      row_one_sum = matrix[row_base][i..i + 2].sum
+      row_two_sum = matrix[row_base + 1][i + 1]
+      row_three_sum = matrix[row_base + 2][i..i + 2].sum
+
+      hourglass_sum = row_one_sum + row_two_sum + row_three_sum
+
+      if hourglass_sum > max_hourglass_sum
+        max_hourglass_sum = hourglass_sum
+      end
+
+      col_base += 1
+    end
+    row_base += 1
+    col_base = 0
+  end
+  return max_hourglass_sum
 end

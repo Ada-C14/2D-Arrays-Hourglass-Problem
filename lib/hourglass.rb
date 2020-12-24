6232c4ac4c2ead2b3b920ddfd0ee64d0a47e12f6
dnsrocha <drocha@gmail.com>
2020-12-24 15:39:51 -0800

    changed method name to reflect the problem statement, change tests for same reason, assigned negative Infinity to sum for enhance the functionality of the method

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,7 +1,7 @@
 
 def hourglass(matrix)
   # raise NotImplementedError, "The matrix method has not been implemented yet!"
-  max = -10000000000000
+  max = -Float::INFINITY
   (0..3).each do |row|
     (0..3).each do |col|
       sum = (matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2] + matrix[row + 1][col + 1] + matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2])

76ce1ee436efa2b0c790d3d97184e8cfe18eb684
dnsrocha <drocha@gmail.com>
2020-12-24 14:52:24 -0800

    implemented hourglass method, most tests passing

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,12 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  # raise NotImplementedError, "The matrix method has not been implemented yet!"
+  max = 0
+  (0..3).each do |row|
+    (0..3).each do |col|
+      sum = (matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2] + matrix[row + 1][col + 1] + matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2])
+      max = sum if sum > max
+    end
+  end
+  return max
 end

d1daf81032e5efc6599508b3c1974e349197fb6b
gessica <gessicamohr18@gmail.com>
2020-12-27 10:24:49 -0800

    added time and space complexity

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,22 @@
+#Time complexity: O(n^2)
+# Space complexity: O(n^2)
+
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  sum = []
+  i = 0
+  j = 0
+
+  (matrix.length - 2).times do |i|
+    (matrix.length - 2).times do |j|
+
+      sum << (matrix[i][j] + matrix[i][j+1] + matrix[i][j+2] +
+          matrix[i+1][j+1] +
+          matrix[i+2][j] + matrix[i+2][j+1] + matrix[i+2][j+2]
+      )
+    end
+  end
+
+  return sum.max
 end
+

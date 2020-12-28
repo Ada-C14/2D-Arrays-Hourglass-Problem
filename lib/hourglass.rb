fff443404e6b2463c658a0ba8e240c487182696c
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-12-28 10:15:55 -0800

    refactor

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,27 +1,3 @@
-def hourglass(matrix)
-  raise ArgumentError, "Invalid matrix" if matrix.nil?
-
-  hg_sums = [] # collect each hourglass sum
-  row_i = 0 # track row index
-  col_i = 0 # track column index
-
-  until hg_sums.size == 16 # hit 16 at the last hourglass
-
-    hg_sums << (matrix[row_i][col_i] + matrix[row_i][col_i+1] + matrix[row_i][col_i+2] +
-        matrix[row_i+1][col_i+1] +
-        matrix[row_i+2][col_i] + matrix[row_i+2][col_i+1] + matrix[row_i+2][col_i+2])
-
-    col_i += 1 # the column index should increment 4x on each row
-
-    if col_i > 3 # get to the end of the row
-      col_i = 0 # reset column index
-      row_i += 1 # shift down to next row (so we start adding from the next top left corner)
-    end
-  end
-
-  return hg_sums.max
-end
-
 # matrix = [[1,1,1,0,0,0], #row_index => 0
 #           [0,1,0,0,0,0], #row_index => 1
 #           [1,1,1,0,0,0], #row_index => 2
@@ -33,7 +9,32 @@ end
 #notes: will always be 6x6 array
 
 #Pseudocode:
-  #find each hourglass
-  #calculate and store each sum in an array
-  #return highest value in that array (holding each hourglass sum)
+#find each hourglass
+#calculate and store each sum in an array
+#return highest value in that array (holding each hourglass sum)
+
+def hourglass(matrix)
+  raise ArgumentError, "Invalid matrix" if matrix.nil?
+
+  hg_sums = [] # collect each hourglass sum
+  row_i = 0 # track row index
+  col_i = 0 # track column index
+
+  until hg_sums.size == 16
+
+    hg_sums << (matrix[row_i][col_i] + matrix[row_i][col_i+1] + matrix[row_i][col_i+2] +
+        matrix[row_i+1][col_i+1] +
+        matrix[row_i+2][col_i] + matrix[row_i+2][col_i+1] + matrix[row_i+2][col_i+2])
+
+    col_i += 1
+
+    if col_i == 4
+      col_i = 0
+      row_i += 1
+    end
+  end
+
+  return hg_sums.max
+end
+
 

28f2344c85f89586cae97bc9ceb97697efde1ebe
beauttie <bak02013@pomona.edu>
2020-12-27 23:51:23 -0800

    Implement hourglass method to pass all tests

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,4 +1,21 @@
 
 def hourglass(matrix)
-  raise NotImplementedError, "The matrix method has not been implemented yet!"
+  hourglass_sum = 0
+  max_sum = nil
+  # shifts hourglass to the right
+  4.times do |i|
+    # calculates sums vertically
+    4.times do |j|
+      3.times do |k|
+        hourglass_sum += matrix[j][i + k]
+        hourglass_sum += matrix[j + 2][i + k]
+      end
+      hourglass_sum += matrix[j + 1][i + 1]
+      # hourglass sums can be all negative
+      max_sum = hourglass_sum if max_sum.nil? || hourglass_sum > max_sum
+      hourglass_sum = 0
+    end
+
+  end
+  return max_sum
 end

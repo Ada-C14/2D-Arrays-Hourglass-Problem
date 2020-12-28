881bc4f5131bbf1d1df4da2aee1a4e1f1b2bc929
beauttie <bak02013@pomona.edu>
2020-12-28 00:05:07 -0800

    DRY up hourglass_sum reference

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -2,13 +2,14 @@
 # Space complexity: O(1)
 
 def hourglass(matrix)
-  hourglass_sum = 0
   max_sum = nil
   # shifts hourglass to the right
   # can use matrix.length - 2 instead of 4
   4.times do |i|
     # calculates sums vertically
     4.times do |j|
+      # resets sum to zero
+      hourglass_sum = 0
       3.times do |k|
         hourglass_sum += matrix[j][i + k]
         hourglass_sum += matrix[j + 2][i + k]
@@ -16,7 +17,6 @@ def hourglass(matrix)
       hourglass_sum += matrix[j + 1][i + 1]
       # hourglass sums can all be negative
       max_sum = hourglass_sum if max_sum.nil? || hourglass_sum > max_sum
-      hourglass_sum = 0
     end
   end
 

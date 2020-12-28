8612f161813be5e51ea04aef5d1aecd9141e7fe1
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-28 09:15:44 -0800

    added time and space complexity notes

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,9 +1,13 @@
-
+#Time complexity: O(1) == O(nm) where n is the number of rows and m is the number of columns in the matrix; for this case, n and m are both fixed values (6), O(nm) = O(36) == O(1)
+# Space complexity: O(1) for the local variables (num_rows, num_columns, max_hourglass, hourglass_sum, and in the helper: row1, row2, row3)
 def hourglass(matrix)
+  num_rows = 6
+  num_columns = 6
   max_hourglass = nil
-  6.times do  |row|
+
+  num_rows.times do  |row|
     if row > 0 && row < 5
-      6.times do |column|
+      num_columns.times do |column|
         if column > 0 && column < 5
           hourglass_sum = hourglass_helper(matrix, [row, column])
           if max_hourglass.nil? || hourglass_sum > max_hourglass

63c145d76fed6be810ac0b83e2b4b2771485a9b1
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-28 16:28:44 -0800

    initial clone, pseudocode/approach for problem

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -1,3 +1,14 @@
+# INFO:
+# We have a 6x6 matrix
+# hourglass is ( 3 at top row, middle element on 2nd row and 3 at bottom row)
+# calculate each hourglass sum
+# return the max hourglass sum
+
+#PSEUDOCODE:
+# inititalize Array to keep track of each hourglass_sum
+# set row/column variables to 0(index)
+# calculate all hourglasses total for each
+# 16 hourglasses total
 
 def hourglass(matrix)
   raise NotImplementedError, "The matrix method has not been implemented yet!"

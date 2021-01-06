b5077f9bd85bd891ec4cd6c446db601c604ec4c7
Jessica Chan <jwinchan@gmail.com>
2021-01-05 17:50:43 -0800

    completed hourglass method

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -3,7 +3,8 @@ def hourglass(matrix)
   sums = []
   (0..3).each do |x|
     (0..3).each do |y|
-      sums << (matrix[x][y] + matrix[x][y+1] + matrix[x][y+2] + matrix[x+1][y+1] + matrix[x+2][y] + matrix[x+2][y+1] + matrix[x+2][y+2])
+      sums << (matrix[x][y] + matrix[x][y + 1] + matrix[x][y + 2] + matrix[x + 1][y + 1] + matrix[x + 2][y]
+              + matrix[x + 2][y + 1] + matrix[x + 2][y + 2])
     end
   end
   return sums.max
